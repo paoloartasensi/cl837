64ce9c1f4ef3665047bc78e02fc295af97bb1b9a
Paolo Artasensi <paoloartasensi@gmail.com>
2025-10-20 14:10:04 +0200

    Remove obsolete scanner classes: ScanSettings, ScannerService, and UserScanCallbackWrapper
    - Deleted ScanSettings.java, ScannerService.java, and UserScanCallbackWrapper.java files as they are no longer needed in the project.

--- REVERSE/no/nordicsemi/android/ble/exception/BluetoothDisabledException.java
+++
@@ -1,9 +0,0 @@
-package no.nordicsemi.android.ble.exception;
-
-public class BluetoothDisabledException extends ConnectionException {}
-
-
-/* Location:              C:\Users\Admin\Downloads\jd-gui-windows-1.6.6\classes.jar!\no\nordicsemi\android\ble\exception\BluetoothDisabledException.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
--- REVERSE/no/nordicsemi/android/ble/exception/DeviceDisconnectedException.java
+++
@@ -1,9 +0,0 @@
-package no.nordicsemi.android.ble.exception;
-
-public class DeviceDisconnectedException extends ConnectionException {}
-
-
-/* Location:              C:\Users\Admin\Downloads\jd-gui-windows-1.6.6\classes.jar!\no\nordicsemi\android\ble\exception\DeviceDisconnectedException.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
